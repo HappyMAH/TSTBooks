72684e4ce33cf7a490b7492f476f4f817e66c485
HappyMAH <khabibullin20024@gmail.com>
2022-05-20 09:56:07 +0300

    first commit

---
+++ Books/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Demo_Lib.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo_Lib
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static LibraryEntities ent = new LibraryEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            LibList.ItemsSource = ent.Book.ToList();
+
+            
+        }
+    }
+}
---
+++ Books/Model/PartialClasses/BookPartial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Demo_Lib.Model
+{
+    public partial class Book
+    {
+        public string GetImage
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Image))
+                    return $"Images\\picture.png";
+                else
+                    return $"Images\\{Image}";
+            }
+        }
+
+        public SolidColorBrush DiscountColor
+        {
+            get
+            {
+                if (Discount > 15)
+                    return (SolidColorBrush)new BrushConverter().ConvertFrom("#7fff00");
+                else
+                    return new SolidColorBrush(Colors.White);
+            }
+        }
+
+        public string GetDiscountCost
+        {
+            get
+            {
+                if (Discount != null && Discount != 0)
+                    return $"{Cost} /{(double)Cost - (double)Cost * ((double)Discount * 0.01)}";
+                else
+                    return Cost.ToString();
+
+            }
+        }
+
+        public Visibility GetVisibility
+        {
+            get
+            {
+                if (Discount != null && Discount != 0)
+                    return Visibility.Visible;
+                else
+                    return Visibility.Hidden;
+            }
+        }
+    }
+}
